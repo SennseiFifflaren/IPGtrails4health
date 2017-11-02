f659e64b628da2abe452989f9724cb482f5e4b40
SennseiFifflaren <32763624+SennseiFifflaren@users.noreply.github.com>
2017-11-02 22:08:31 +0000

    fix

--- IPGtrails4health/IPGtrails4health/obj/Debug/netcoreapp1.1/IPGtrails4health.AssemblyInfo.cs
+++ IPGtrails4health/IPGtrails4health/obj/Debug/netcoreapp1.1/IPGtrails4health.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPGtrails4health")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPGtrails4health")]
