27cb9907e919aa200cf3df97242699b7038752ad
ruiaraujo012 <ruiaraujo012@gmail.com>
2017-11-10 12:57:32 +0000

    Criação do formulário Inserir Pontos de Interesse
    Tempo: 2 horas

--- IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Models/TurismoViewModels/PontosInteresseViewModel.cs
+++ IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Models/TurismoViewModels/PontosInteresseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace IPGTrails4Health.Models.TurismoViewModels
 {
     public class PontosInteresseViewModel
     {
-        public string PontoInteresse { get; set; }
+        [Required(ErrorMessage = "Por favor escolha uma opção")]
+        public int PontoInteresse { get; set; }
+
+        [Required(ErrorMessage = "Por favor introduza o nome do ponto de interesse")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor introduza o local do ponto de interesse")]
         public string Local { get; set; }
-        public string Sazonalidade { get; set; }
+
+        [Required(ErrorMessage = "Por favor escolha uma opção")]
+        public int Sazonalidade { get; set; }
     }
 }
