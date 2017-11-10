f791f4bc7e91b7b41855075272ca25f9f1862e0c
Leandro <fernandesleandro1996@gmail.com>
2017-11-10 12:58:02 +0000

    Criaçao do formulario para criar trilho
    Tempo despendido: 2h

---
+++ IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Models/TrilhosViewModels/CriarTrilhoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPGTrails4Health.Models.TrilhosViewModels
+{
+    public class CriarTrilhoViewModel
+    {
+        public String Nome { get; set; }
+        public String Partida { get; set; }
+        public String Chegada { get; set; }
+        public int Distancia { get; set; }
+        public int Dificuldade { get; set; }
+        public bool Percurso { get; set; }
+
+    }
+}
