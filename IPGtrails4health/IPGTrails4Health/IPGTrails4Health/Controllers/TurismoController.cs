e5bd39465bc1d3379c8bec39fbd11a7b48bac96b
ruiaraujo012 <ruiaraujo012@gmail.com>
2017-11-11 17:29:13 +0000

    Formatação do formulário Inserir Pontos de Interesse e validações dos campos (Tempo: 1h30m)

--- IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Controllers/TurismoController.cs
+++ IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Controllers/TurismoController.cs
@@ -2,17 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IPGTrails4Health.Models.TurismoViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IPGTrails4Health.Controllers
 {
     public class TurismoController : Controller
     {
-        public IActionResult PontosInteresse()
+        [HttpGet]
+
+        public ViewResult PontosInteresse()
         {
             ViewData["Message"] = "Página para adicionar pontos de interesse (fauna, flora, históricos, monumentos) tendo em conta a sazonalidade.";
             return View();
         }
+
+        [HttpPost]
+
+        public ViewResult PontosInteresse(PontosInteresseViewModel dados)
+        {
+            if (ModelState.IsValid)
+            {
+                //Repository.AddGuestResponse(response);
+                return View("Inserido", dados);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
         public IActionResult Extras()
         {
             ViewData["Message"] = "Página para inserir alojamento, restaurantes, áreas de descanso, WC ao longo do percurso.";
--- IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Models/TurismoViewModels/PontosInteresseViewModel.cs
+++ IPGtrails4health/IPGTrails4Health/IPGTrails4Health/Models/TurismoViewModels/PontosInteresseViewModel.cs
@@ -8,16 +8,22 @@ namespace IPGTrails4Health.Models.TurismoViewModels
 {
     public class PontosInteresseViewModel
     {
-        [Required(ErrorMessage = "Por favor escolha uma opção")]
+        [Required(ErrorMessage = "Por favor escolha a categoria do ponto de interesse")]
+        [RegularExpression("[1-9]+", ErrorMessage = "Por favor escolha a categoria do ponto de interesse")]
         public int PontoInteresse { get; set; }
 
         [Required(ErrorMessage = "Por favor introduza o nome do ponto de interesse")]
+        [RegularExpression(@"[a-zA-Z\s]+", ErrorMessage = "Nome do ponto de interesse inválido")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Por favor introduza o local do ponto de interesse")]
+        [RegularExpression(@"[a-zA-Z\s]+", ErrorMessage = "Local do ponto de interesse inválido")]
         public string Local { get; set; }
 
-        [Required(ErrorMessage = "Por favor escolha uma opção")]
+        [Required(ErrorMessage = "Por favor escolha a sazonalidade")]
+        [RegularExpression("[1-9]", ErrorMessage = "Por favor escolha a sazonalidade")]
         public int Sazonalidade { get; set; }
+
+        public string Observacoes { get; set; }
     }
 }
